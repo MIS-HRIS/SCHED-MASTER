c296c480800c039c93e6adec1eed08b32a149605
MIS-HRIS <mpbugas@gmail.com>
2025-10-22 13:59:43 +0000

    update

--- app.js
+++ app.js
@@ -110,11 +110,11 @@ function handlePaste(event) {
     date: d.date ? excelDateToJS(d.date) : ''
   }));
 
-  if (isWork) {
-    workScheduleData = processedData;
-  } else {
-    restDayData = processedData;
-  }
+if (isWork) {
+  workScheduleData = [...(workScheduleData || []), ...processedData];
+} else {
+  restDayData = [...(restDayData || []), ...processedData];
+}
 
   recheckConflicts();
   updateButtonStates();
